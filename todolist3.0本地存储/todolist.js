55694d530f231ffcf9c8f9a9386abdbf9fb0f8fb
“fanyihua0309” <nuaaccstfyh@163.com>
2021-04-13 22:13:59 +0800

    优化代码，然后出现了一大堆问题

--- todolist3.0本地存储/todolist.js
+++ todolist3.0本地存储/todolist.js
@@ -1,93 +1,100 @@
 let curId = 0;    //记录当前待办事项的id值
 
-let obj = {     //定义存储对象
-    content: "test",
-    complete: true,
-    id: 0
-};
-
-let objArray = JSON.parse(localStorage.getItem("待办事项"));  
-if(objArray === null)   //若localStorage中不存在待办事项,objArray为null
-    objArray = [];      //则将值置为空数组,否则无法使用push等数组操作方法
-
-localStorage.setItem("待办事项", JSON.stringify(objArray));
-
-// window.onload表示在页面加载时即执行readData函数,从localStorage中读取已有的待办事项
+/**
+ * window.onload表示在页面加载时即执行readData函数
+ * 从localStorage中读取已有的待办事项
+ */
 window.onload =  function readData(){  
-    objArray = JSON.parse(localStorage.getItem("待办事项")); 
-    curId = objArray.length;    //设置新增的第一个待办事项的id
-    for(let i = 0; i < objArray.length; i++){
-        let curObj = objArray[i];
-        if(curObj.id !== -1){       //id值为-1表示被删除的待办事项,不处理
-            renderTodoList(curObj.content, curObj.complete, curObj.id, false);
+    let itemList = JSON.parse(localStorage.getItem("待办事项"));  
+    if(itemList === null)   //若localStorage中不存在待办事项,itemList为null
+        itemList = [];      //则将值置为空数组,否则无法使用push等数组操作方法
+    localStorage.setItem("待办事项", JSON.stringify(itemList));
+
+    itemList = JSON.parse(localStorage.getItem("待办事项")); 
+    curId = itemList.length;    //设置新增的第一个待办事项的id
+    for(let i = 0; i < itemList.length; i++){
+        const curItem = itemList[i];
+        // console.log(curId);
+        // 做容错处理：先判断 curItem 是否存在，如果不存在程序都没必要往下进行
+        if (!curItem) {
+            console.error("没有取得当前待办事项元素，初始化失败！");
+            return;
         }
+        // if(curItem.id !== -1){       //id值为-1表示被删除的待办事项,不处理
+            renderTodoList(curItem.content, curItem.complete, curItem.id, false);
+        // }
     }
 }
 
-//按下回车键提交进行新增
+/**
+ * 按下回车键提交进行新增
+ * @param {number} keyCode 用户按下的按键的键值
+ */
 function enterToSubmit(keyCode){     
     if (keyCode === 13){  //回车键的键值为13
         handleSubmit();
     }
 }
 
-//按下回车键提交进行查询
+/**
+ * 按下回车键提交进行查询
+ * @param {number} keyCode 用户按下的按键的键值
+ */
 function enterToSearch(keyCode){    
     if (keyCode === 13){  //回车键的键值为13
         handleSearch();
     }
 }
 
-//处理用户提交的表单
+/**
+ * 处理用户提交的表单
+ */
 function handleSubmit(){
     const form = document.querySelector("form"); 
     const formData = new FormData(form);
     const todoItem = formData.get("todo-item");
 
-    renderTodoList(todoItem, false, 0, true);
-    afterSubmit();
-}
+    // 如果用户在创建输入框中的内容是空的，就不处理（否则会出现一条空的待办事项）
+    let spaces = "^[ ]+$";  //多个空格也判为空
+    var re = new RegExp(spaces);
+    if(re.test(todoItem) === true){
+        alert("待办事项为空，无法创建!");
+        return;
+    }
 
-//提交表单之后的操作
-function afterSubmit(){
+    renderTodoList(todoItem, false, 0, true);
     document.querySelector("#item").value = "";
     document.querySelector("#item").focus();
 }
 
-//新创建一个以buttonName为名的按钮
-function createButton(buttonName){      
-    const button = document.createElement("button");
-    button.innerHTML = buttonName;
-    button.className = "button";
-    return button;
-}
-
-//渲染出用户的待办事项
-//传入的参数中todoItem为待办事项名称
-//complete为true/false表示是否完成
-//id为待办事项的id
-//newObj为true/false表示当前要进行渲染的待办事项为新增的还是从localStorage中读取的旧值
-function renderTodoList(todoItem, complete, id, newObj){
+/**
+ * 渲染出用户的待办事项
+ * @param {string} todoItem 待办事项名称
+ * @param {boolean} complete 表示是否完成
+ * @param {number} id 待办事项的id
+ * @param {boolean} newObj 当前要进行渲染的待办事项为新增的还是从localStorage中读取的旧值
+ */
+ function renderTodoList(todoItem, complete, id, newObj){
     let tbody = document.getElementById("todo-list-body");
     let tr = document.createElement("tr");
     let td = document.createElement("td");
     let span = document.createElement("span");
     span.innerHTML = todoItem;
-    span.className = "todo-list-left";
     
     td.appendChild(span);
-    // td.style.width = "60%";
     tr.appendChild(td);
     tbody.appendChild(tr);
 
-    if(newObj == true){
-        obj.content = todoItem;
-        obj.complete = false;   //新增的待办事项默认为未完成
-        obj.id = curId;
-        
-        objArray = JSON.parse(localStorage.getItem("待办事项"));
-        objArray.push(obj);
-        localStorage.setItem("待办事项", JSON.stringify(objArray));
+    if(newObj === true){
+        let oneItem = new Object();
+        oneItem.content = todoItem;
+        oneItem.complete = false;   //新增的待办事项默认为未完成
+        oneItem.id = curId;
+
+        console.log(oneItem);
+        let itemList = JSON.parse(localStorage.getItem("待办事项"));
+        itemList.push(oneItem);
+        localStorage.setItem("待办事项", JSON.stringify(itemList));
 
         tr.id = String(curId);
         curId++;
@@ -95,93 +102,188 @@ function renderTodoList(todoItem, complete, id, newObj){
     else{
         tr.id = String(id);
     }
-
     span.id = tr.id + "span";
 
+    // console.log(tr.id);
+    // console.log(span.id);
+    // console.log(todoItem);
+    // console.log(complete);
+    createOperationTd(tr.id, span.id, todoItem, complete);
+}
+
+/**
+ * 创建当前行的第二列,创建三个操作按钮并实现相应功能
+ * @param {string} trId 待操作行tr的id
+ * @param {string} spanId 存储待办事项内容的标签span的id
+ * @param {string} todoItem 待操作的待办事项的内容
+ * @param {boolean} complete 当前待办事项是否完成
+ */
+ function createOperationTd(trId, spanId, todoItem, complete){
+    const tr = document.getElementById(trId);
+    const span = document.getElementById(spanId);
     let tdButton = document.createElement("td");
-    tdButton.className = "todo-list-right";
 
-    //--------------------------------------------------编 辑--------------------------------------------------
-    const buttonEdit = createButton("编辑");    //新增编辑按钮
-    let inputEdit = document.createElement("input");    //新增标签input,用于编辑功能用户输入编辑的内容
-    inputEdit.type = "text";
-    inputEdit.id = "input-edit-text";
-    inputEdit.value = todoItem;
-    tr.cells[0].appendChild(inputEdit);
+    //1.编辑
+    // const buttonEdit = createButton("编辑");    //新增编辑按钮
+    // let inputEdit = document.createElement("input");    //新增标签input,用于编辑功能用户输入编辑的内容
+    // inputEdit.type = "text";
+    // inputEdit.id = trId + "input-edit-text";
+    // inputEdit.value = todoItem;
+    // inputEdit.style.display = "none";
+    // tr.cells[0].appendChild(inputEdit);
 
-    const buttonSubmit = createButton("提交");  //新增提交按钮
-    buttonSubmit.style.display = "none";
-    tr.cells[0].appendChild(buttonSubmit);
+    // const buttonSubmit = createButton("提交");  //新增提交按钮
+    // buttonSubmit.id = trId + "button-submit-edit";
+    // buttonSubmit.style.display = "none";
+    // tr.cells[0].appendChild(buttonSubmit);
 
-    buttonEdit.onclick = function handleEdit(){   //处理用户点击编辑按钮事件
-        inputEdit.style.display = "inline";       //点击编辑按钮后显示输入框
-        buttonSubmit.style.display = "inline";
-        tr.cells[0].firstChild.style.display = "none";
-        
-        buttonSubmit.onclick = function hideInputEdit(){    //点击提交按钮表示用户输入编辑后的内容完毕
-            let editText = inputEdit.value;
-            tr.cells[0].firstChild.innerHTML = editText;
-            tr.cells[0].firstChild.style.display = "inline";
-            inputEdit.style.display = "none";
-            buttonSubmit.style.display = "none";
+    // buttonEdit.onclick = handleEdit(trId, inputEdit.id, buttonSubmit.id);
+    // tdButton.appendChild(buttonEdit);
 
-            //更改localStorage中相应的待办事项
-            objArray = JSON.parse(localStorage.getItem("待办事项"));    
-            let curObj = objArray[Number(tr.id)];   
-            curObj.content = editText;
-            localStorage.setItem("待办事项", JSON.stringify(objArray));
-        }
-    }  
-    tdButton.appendChild(buttonEdit);
-
-    //--------------------------------------------------删 除--------------------------------------------------
+    //2.删除
     const buttonDelete = createButton("删除");       //新增删除按钮
-    buttonDelete.onclick = function handleDelete(){   //处理用户点击删除按钮事件
-        tr.remove();
-
-        //删除localStorage中相应的待办事项(不是真正删除，只是将id置为不可用值)
-        objArray = JSON.parse(localStorage.getItem("待办事项"));    
-        let curObj = objArray[Number(tr.id)];   
-        curObj.id = -1;     //id置为-1表示不再使用该条待办事项记录
-        localStorage.setItem("待办事项", JSON.stringify(objArray));
-    }
+    buttonDelete.onclick = handleDelete(trId);
     tdButton.appendChild(buttonDelete);
 
-    //--------------------------------------------------完 成--------------------------------------------------
-    let buttonComplete;
-    if(complete === false){
-        document.getElementById(span.id).style.textDecoration = "none";
-        buttonComplete = createButton("完成");    //新增完成按钮
-    }
-    else if(complete === true){
-        document.getElementById(span.id).style.textDecoration = "line-through";
-        buttonComplete = createButton("未完成");    //新增未完成按钮 
-    }
+    //3.完成/未完成
+    // const buttonComplete = createButton("完成");
+    // if(complete === false){
+    //     span.style.textDecoration = "none";
+    //     // buttonComplete = createButton("完成");    //新增完成按钮
+    // }
+    // else if(complete === true){
+    //     span.style.textDecoration = "line-through";
+    //     // buttonComplete = createButton("未完成");    //新增未完成按钮 
+    //     buttonComplete.innerHTML = "未完成";
+    // }
+    // buttonComplete.id = "button-complete" + trId;
+    // buttonComplete.onclick = handleComplete(trId, spanId, "button-submit-complete");
 
-    buttonComplete.onclick = function handleComplete(){   //处理用户点击完成/未完成按钮事件
-        objArray = JSON.parse(localStorage.getItem("待办事项"));    
-        let curObj = objArray[Number(tr.id)];
-        if(buttonComplete.innerHTML === "完成"){
-            document.getElementById(span.id).style.textDecoration = "line-through";
-            buttonComplete.innerHTML = "未完成";
-
-            curObj.complete = true;    //变更localStorage中相应的待办事项为完成状态
-        }
-        else{
-            document.getElementById(span.id).style.textDecoration = "none";
-            buttonComplete.innerHTML = "完成";
-            
-            curObj.complete = false;     //变更localStorage中相应的待办事项为未完成状态
-        }
-        localStorage.setItem("待办事项", JSON.stringify(objArray));  
-    }
-    tdButton.appendChild(buttonComplete);
-    // tdButton.style.width = "40%";
-
+    // console.log(buttonComplete);
     tr.appendChild(tdButton);
 }
 
-//查询功能
+/**
+ * 新创建一个以buttonName为名的按钮
+ * @param {string} buttonName 按钮名称
+ * @returns button元素
+ */
+ function createButton(buttonName){      
+    const button = document.createElement("button");
+    button.innerHTML = buttonName;
+    button.className = "button";
+    return button;
+}
+
+/**
+ * 当用户输入编辑后的内容点击提交时
+ * @param {string} trId 待操作行tr的id
+ * @param {string} inputEditId 点击编辑按钮后出现的输入框的id
+ * @param {string} buttonSubmitId 点击编辑按钮后出现的提交按钮的id
+ */
+function handleSubmitEdit(trId, inputEditId, buttonSubmitId){    //点击提交按钮表示用户输入编辑后的内容完毕
+    const tr = document.getElementById(trId);
+    const inputEdit = document.getElementById(inputEditId);
+    const buttonSubmit = document.getElementById(buttonSubmitId);
+    let editText = inputEdit.value;
+    tr.cells[0].firstChild.innerHTML = editText;
+    tr.cells[0].firstChild.style.display = "inline";
+    inputEdit.style.display = "none";
+    buttonSubmit.style.display = "none";
+
+    更改localStorage中相应的待办事项
+    let itemList = JSON.parse(localStorage.getItem("待办事项")); 
+    console.log(itemList);   
+    let curItem = itemList[Number(trId)]; 
+    if (!curItem) {
+        console.error("没有取得当前待办事项元素，初始化失败！");
+        return;
+    }
+    curItem.content = editText;
+    localStorage.setItem("待办事项", JSON.stringify(itemList));
+}
+
+/**
+ * 处理用户点击编辑按钮事件
+ * @param {string} trId 待操作行tr的id
+ * @param {string} inputEditId 点击编辑按钮后出现的输入框的id
+ * @param {string} buttonSubmitId 点击编辑按钮后出现的提交按钮的id
+ */
+function handleEdit(trId, inputEditId, buttonSubmitId){   
+    const tr = document.getElementById(trId);
+    const inputEdit = document.getElementById(inputEditId);
+    const buttonSubmit = document.getElementById(buttonSubmitId);
+
+    console.log(inputEdit);
+    console.log(buttonSubmit);
+
+    
+
+    inputEdit.style.display = "inline";       //点击编辑按钮后显示输入框
+    buttonSubmit.style.display = "inline";
+    tr.cells[0].firstChild.style.display = "none";
+    
+    console.log(inputEdit);
+    buttonSubmit.onclick = handleSubmitEdit(trId, inputEditId, buttonSubmitId);
+}  
+
+
+/**
+ * 处理用户点击删除按钮事件
+ * @param {string} trId 待删除行的tr元素的id
+ */
+function handleDelete(trId){   
+    const tr = document.getElementById(trId);
+    tr.remove();
+
+    //删除localStorage中相应的待办事项(不是真正删除，只是将id置为不可用值)
+    let itemList = JSON.parse(localStorage.getItem("待办事项"));    
+    let curItem = itemList[Number(trId)];  
+    if (!curItem) {
+        console.error("没有取得当前待办事项元素，初始化失败！");
+        return;
+    }
+    console.log("删除");
+    console.log(curItem);
+    curItem.id = -1;     //id置为-1表示不再使用该条待办事项记录
+    localStorage.setItem("待办事项", JSON.stringify(itemList));
+}
+
+/**
+ * 处理用户点击完成/未完成按钮事件
+ * @param {string} trId 待操作行tr的id
+ * @param {string} spanId 存储待办事项内容的标签span的id
+ */
+function handleComplete(trId, spanId, buttonCompleteId){   
+    const buttonComplete = document.getElementById(buttonCompleteId);
+    const span = document.getElementById(spanId);
+
+    console.log(buttonComplete);
+
+    let itemList = JSON.parse(localStorage.getItem("待办事项"));    
+    let curItem = itemList[Number(trId)];
+    if (!curItem) {
+        console.error("没有取得当前待办事项元素，初始化失败！");
+        return;
+    }
+    if(buttonComplete.innerHTML === "完成"){
+        span.style.textDecoration = "line-through";
+        buttonComplete.innerHTML = "未完成";
+
+        curItem.complete = true;    //变更localStorage中相应的待办事项为完成状态
+    }
+    else{
+        span.style.textDecoration = "none";
+        buttonComplete.innerHTML = "完成";
+        
+        curItem.complete = false;     //变更localStorage中相应的待办事项为未完成状态
+    }
+    localStorage.setItem("待办事项", JSON.stringify(itemList));  
+}
+
+/**
+ * 处理用户的查询待办事项操作
+ */
 function handleSearch(){
     const searchText = document.getElementById("search-text").value;    //用户输入的待查询文本
     const tbody = document.getElementById("todo-list-body");  
